82237de46c25fdb81318d368cba852dc31e86588
Tiago Pereira <tmpbruk@gmail.com>
2024-04-27 18:57:53 +0100

    feat: add input component

---
+++ components/Input.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+interface InputProps {
+  placeholder?: string;
+  value?: string;
+  type?: string;
+  disabled?: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+export const Input: React.FC<InputProps> = ({
+  placeholder,
+  value,
+  type,
+  disabled,
+  onChange,
+}) => {
+  return (
+    <input
+      value={value}
+      type={type}
+      placeholder={placeholder}
+      disabled={disabled}
+      onChange={onChange}
+      className="
+        w-full
+        p-4
+        text-lg
+        bg-black
+        border-2
+        border-neutral-800
+        rounded-md
+        outline-none
+        text-white
+        focus:border-sky-500
+        focus:border-2
+        transition
+        disabled:bg-neutral-900
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+      "
+    />
+  );
+};
