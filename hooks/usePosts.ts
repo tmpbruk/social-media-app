5f562d4cce34e366bbcc9147980e463bc4e07385
Tiago Pereira <tmpbruk@gmail.com>
2024-05-03 09:24:54 +0100

    feat: add useposts and usepost hooks

--- hooks/usePosts.ts
+++ hooks/usePosts.ts
@@ -6,9 +6,8 @@ import fetcher from "@/libs/fetcher";
 // We can re-use useCurrent in different place, but it is not going to refetch every time we use it.
 // It will check and see if data already exists and will decide if data needs to be fetched again.
 // This will replace our global state e.g redux.
-const usePosts = (userId?: string) => {
-  const url = userId ? `/api/posts?userId=${userId}` : "/api/posts";
-  const { data, error, isLoading, mutate } = useSWR(url, fetcher);
+const usePosts = () => {
+  const { data, error, isLoading, mutate } = useSWR("/api/posts", fetcher);
 
   return { data, error, isLoading, mutate };
 };
