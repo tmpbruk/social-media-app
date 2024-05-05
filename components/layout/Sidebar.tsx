5530b3c6003214f810373ea0b7222eba854cbd49
Tiago Pereira <tmpbruk@gmail.com>
2024-05-05 15:20:38 +0100

    feat: add notifications to sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -22,6 +22,7 @@ export const Sidebar = () => {
       href: "/notifications",
       icon: BsBellFill,
       auth: true,
+      alert: currentUser?.hasNotification,
     },
     {
       label: "Profile",
@@ -43,6 +44,7 @@ export const Sidebar = () => {
               label={item.label}
               icon={item.icon}
               auth={item.auth}
+              alert={item.alert}
             />
           ))}
           {currentUser && (
